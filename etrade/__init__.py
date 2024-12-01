3c1d08f6a513780ed016a45bbfcd2c7c06593898
Kürşat <draknes.gamin70@gmail.com>
2024-12-01 12:40:30 +0300

    fix cors errors

--- etrade/__init__.py
+++ etrade/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from etrade.routes import register_routes
 from etrade.config import Config
 from etrade.extensions import socketio
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app) 
     app.config.from_object(Config)
 
     socketio.init_app(app)
