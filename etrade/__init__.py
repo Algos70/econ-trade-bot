61a4882ebc4043efff4d2324aadb6c9e1cba7068
Kürşat <draknes.gamin70@gmail.com>
2024-12-02 20:42:37 +0300

    __init__ statical analysis improvement

--- etrade/__init__.py
+++ etrade/__init__.py
@@ -2,14 +2,17 @@
 This module contains the main application for the econ-trade-bot API.
 """
 
+# pylint: disable=import-error
 from flask import Flask
+# pylint: disable=import-error
+from flask_cors import CORS
 from etrade.routes import register_routes
 from etrade.config import Config
 from etrade.extensions import socketio
-from flask_cors import CORS
 
 
 def create_app():
+    """Create the Flask app."""
     app = Flask(__name__)
     CORS(app, resources={r"/api/*": {"origins": "*"}})
     app.config.from_object(Config)
