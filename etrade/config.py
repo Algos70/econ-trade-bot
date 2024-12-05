f39bf5f6c10e98ffdb0fa29820b1f3f1c415fbf0
Kürşat <draknes.gamin70@gmail.com>
2024-12-05 14:59:45 +0300

    Deleted api keys are removed

--- etrade/config.py
+++ etrade/config.py
@@ -7,5 +7,5 @@ class Config: # pylint: disable=too-few-public-methods
     """Configuration class for the econ-trade-bot API."""
 
     DEBUG = True
-    API_KEY = "RLro7vCjhhhyet8dY7fBjAwvEWalSQBgrnhPCwBjX1vWiOQujgQ51KuEoyH2SnNM"
-    API_SECRET = "Aq8BccImuXGNzlD3EFv9Lh4h0sQnSgWIZJrbCOvCXIZslcKeCM3hXVJ6sgjc0Fi0"
+    API_KEY = ""
+    API_SECRET = ""
